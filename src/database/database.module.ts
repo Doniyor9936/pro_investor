9e13cad7c6e42c31dbfbd1dbb89f3273ea2ca7e1
Doniyor Qalandarov <doniyorqalandarov853@gmail.com>
2025-09-19 23:06:55 +0500

    bad

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -10,11 +10,12 @@ import { User } from '../user/user.entity';
       inject: [ConfigService],
       useFactory: async (config: ConfigService) => ({
         type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USERNAME'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_NAME'),
+        url: config.get<string>('POSTGRES'),
+        // host: config.get<string>('DB_HOST'),
+        // port: config.get<number>('DB_PORT'),
+        // username: config.get<string>('DB_USERNAME'),
+        // password: config.get<string>('DB_PASSWORD'),
+        // database: config.get<string>('DB_NAME'),
         entities: [User],
         synchronize: true, // development uchun
       }),

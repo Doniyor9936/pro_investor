358186bb7312091c649ba7a74ab034bd5066352f
Doniyor Qalandarov <doniyorqalandarov853@gmail.com>
2025-10-05 15:26:57 +0500

    dsds

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,26 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private configService: ConfigService) {
-    const secret = configService.get<string>('JWT_SECRET');
-    if (!secret) {
-      throw new Error('JWT_SECRET environment o‘zgaruvchisi aniqlanmagan!');
-    }
-
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: secret,
+      secretOrKey: process.env.JWT_SECRET,
     });
   }
 
   async validate(payload: any) {
-    console.log(payload);
-    
-    return {id:payload.userId, userId: payload.userId, email: payload.email };
+    return { id: payload.userId, userId: payload.userId, email: payload.email };
   }
 }
